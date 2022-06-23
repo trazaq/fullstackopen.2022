487308b0e941d0f8aef602d6dd2e3982e3d5333b
Tipu Razaq <tipu.razaq@utexas.edu>
2022-06-23 17:47:03 -0500

    part2: phonebook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {nanoid} from "nanoid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Numbers = ({persons}) => {
+    return (
+        <>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map((p, i) => <li key={nanoid()}>{p.name}</li>)}
+            </ul>
+        </>
+    )
 }
 
-export default App;
+const App = () => {
+    const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+    const [newName, setNewName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (newName.trim().length > 0) {
+            setPersons(persons.concat({name: newName}))
+            setNewName('')
+        }
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name: <input onChange={(e) => setNewName(e.target.value)} value={newName}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <Numbers persons={persons}/>
+        </div>
+    )
+}
+
+export default App

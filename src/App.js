09317e79b4bc2cb81edc5f1c5d30eac7a3630823
trazaq <tr59210@gmail.com>
2022-06-23 19:40:11 -0500

    Phonebook step 3 complete

--- src/App.js
+++ src/App.js
@@ -6,21 +6,29 @@ const Numbers = ({persons}) => {
         <>
             <h2>Numbers</h2>
             <ul>
-                {persons.map((p, i) => <li key={nanoid()}>{p.name}</li>)}
+                {persons.map((p) => <li key={nanoid()}>{p.name} {p.number}</li>)}
             </ul>
         </>
     )
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+    const [persons, setPersons] = useState([{name: 'Arto Hellas', number: '000-000-0000'}])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (newName.trim().length > 0) {
-            setPersons(persons.concat({name: newName}))
-            setNewName('')
+        let name = newName.trim();
+        if (name.length > 0) {
+            //prevent duplicates and alert
+            if (persons.find(e => e.name.toLowerCase() === name.toLowerCase())) {
+                alert(`${name} is already added to the phonebook`)
+            } else {
+                setPersons(persons.concat({name: name, number: newNumber}))
+                setNewName('')
+                setNewNumber('')
+            }
         }
     }
 
@@ -32,6 +40,11 @@ const App = () => {
                     name: <input onChange={(e) => setNewName(e.target.value)} value={newName}/>
                 </div>
                 <div>
+                    <br/>
+                    number: <input onChange={(e) => setNewNumber(e.target.value)} value={newNumber}/>
+                </div>
+                <div>
+                    <br/>
                     <button type="submit">add</button>
                 </div>
             </form>

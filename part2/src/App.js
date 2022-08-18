9851704e64e6b6bc8b0bb6f54c75a2c22f4bb17f
trazaq <tr59210@gmail.com>
2022-08-17 20:06:16 -0500

    updated useEffect so entries are sorted by name

--- part2/src/App.js
+++ part2/src/App.js
@@ -192,7 +192,19 @@ const App = () => {
     //Need to make sure the Express Server is running
     useEffect(() => {
         personService.getAll().then(data => {
-            setPersons(persons.concat(data))
+            setPersons(persons.concat(data).sort((a, b) => {
+                const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+
+                // names must be equal
+                return 0;
+            }))
         })
         /* let data = []
          for (let i = 0; i < 200; i++) {
